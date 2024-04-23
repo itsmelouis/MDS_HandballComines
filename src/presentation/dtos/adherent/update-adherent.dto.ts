a573fb1e18f8880151cfe4eaf7596a57e6455fc0
distrib-ps <theodelhay@orange.fr>
2024-04-23 16:26:50 +0200

    feat:dto update

--- src/presentation/dtos/adherent/update-adherent.dto.ts
+++ src/presentation/dtos/adherent/update-adherent.dto.ts
@@ -1,4 +1,10 @@
-import { IsDate, IsOptional, IsString, MaxLength } from 'class-validator';
+import {
+  IsDate,
+  IsEmail,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class UpdateAdherentDto {
   @IsOptional()
@@ -12,9 +18,18 @@ export class UpdateAdherentDto {
   prenom?: string;
 
   @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  // Password should only be updated through a specific process (e.g., Change Password feature)
+  // Therefore, it's not included in the general update DTO
+
+  @IsOptional()
   @IsDate()
   date_inscription?: Date;
 
-  @IsOptional()
-  roleId?: number;
+  // Role ID might not be updatable via DTO, depending on your application's logic
+  // If roles can be updated, uncomment the following line
+  // @IsOptional()
+  // roleId?: number;
 }
