707f349e6e5f1ed2c0486edf549e32cc0a223c42
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-04-23 14:55:15 +0200

    feat: add `findOne`, `update` and `delete`function to adherent service

--- src/data/services/adherent/adherent.service.ts
+++ src/data/services/adherent/adherent.service.ts
@@ -13,7 +13,12 @@ export class AdherentService {
   async findAll(): Promise<AdherentEntity[]> {
     return this.adherentRepository.find();
   }
-  async createAdherent(adherent: Adherent): Promise<AdherentEntity> {
+  async findOne(id: number): Promise<AdherentEntity> {
+    return this.adherentRepository.findOne({
+      where: { id: id },
+    });
+  }
+  async create(adherent: Adherent): Promise<AdherentEntity> {
     const adherentEntity = this.adherentRepository.create({
       nom: adherent.nom,
       prenom: adherent.prenom,
@@ -21,5 +26,15 @@ export class AdherentService {
       role: adherent.role,
     });
     return this.adherentRepository.save(adherentEntity);
+  }
+  async update(id: number, adherent: Adherent): Promise<AdherentEntity> {
+    const adherentEntity = await this.adherentRepository.findOne({
+      where: { id: id },
+    });
+    return this.adherentRepository.save({ ...adherentEntity, ...adherent });
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.adherentRepository.delete(id);
   }
 }
