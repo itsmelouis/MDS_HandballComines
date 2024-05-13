464994e27526a5e9d98526e4f7080c777873d45d
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-05-13 10:05:42 +0200

    fix: lint errors and type error

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -3,20 +3,9 @@ import { DataModule } from 'src/data/data.module';
 import { CreateMatchUsecase } from './usecase/matchs/create_matchs_usecase';
 import { GetAllMatchUsecase } from './usecase/matchs/get_all_matchs_usecase';
 
-
-
 @Module({
-    imports: [DataModule],
-    providers: [
-        CreateMatchUsecase,
-        GetAllMatchUsecase
-        
-
-    ],
-    exports: [
-        CreateMatchUsecase,
-        GetAllMatchUsecase
-    ]
-
+  imports: [DataModule],
+  providers: [CreateMatchUsecase, GetAllMatchUsecase],
+  exports: [CreateMatchUsecase, GetAllMatchUsecase],
 })
-export class DomainModule { }
+export class DomainModule {}
--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -1,12 +1,11 @@
-import { Module, Get } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { DomainModule } from 'src/domain/domain.module';
-import { MatchController } from './matchs/match.controller';
-import { MatchService } from 'src/data/services/match.service';
-import { MatchEntity } from 'src/data/entities/match.entity';
+import { MatchController } from './controllers/matchs/match.controller';
+// import { MatchService } from 'src/data/services/match.service';
+// import { MatchEntity } from 'src/data/entities/match.entity';
 
 @Module({
-  imports:[DomainModule],
-  controllers:[MatchController]
-
+  imports: [DomainModule],
+  controllers: [MatchController],
 })
 export class PresentationModule {}
